0f2376a471f0110f0e922e575b2fb565b8d74e3f
ilovetux <me@ilovetux.com>
2018-05-30 12:59:44 -0400

    Create writer threadpool to seperate TCP reads from logging calls

--- slither/plugins/syslog_server.py
+++ slither/plugins/syslog_server.py
@@ -5,6 +5,9 @@ import atexit
 import logging
 import click
 import socketserver
+from threading import Thread
+from multiprocessing import JoinableQueue
+from concurrent.futures import ThreadPoolExecutor
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     def get_request(self):
@@ -27,7 +30,21 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
         self.shutdown()
         return SocketServer.TCPServer.server_close(self)
 
-def syslog_server(host, port, poll_interval=0.5, loggername="slither.syslog", certfile=None, keyfile=None):
+def log_writer(q):
+    while True:
+        try:
+            loggername, msg = q.get()
+        except:
+            continue
+        logging.getLogger(loggername).info(msg.strip().decode())
+        q.task_done()
+
+def syslog_server(host, port, poll_interval=0.5, loggername="slither.syslog", max_writers=5, certfile=None, keyfile=None):
+    q = JoinableQueue()
+    writers = [Thread(target=log_writer, args=(q,)) for x in range(max_writers)]
+    for writer in writers:
+        writer.daemon = True
+        writer.start()
     port = int(port)
     poll_interval = float(poll_interval)
     logging.getLogger(loggername).warn("Listening for syslog over TCP on: {}:{} polling: {}".format(host, port, poll_interval))
@@ -35,14 +52,12 @@ def syslog_server(host, port, poll_interval=0.5, loggername="slither.syslog", ce
     class ThreadedTCPRequestHandler(socketserver.StreamRequestHandler):
 
         def handle(self):
-            log = logging.getLogger(
-                "{}.{}".format(
-                    loggername,
-                    self.client_address[0].encode().decode()
-                )
+            _loggername = "{}.{}".format(
+                loggername,
+                self.client_address[0].encode().decode()
             )
             for line in self.rfile:
-                log.info(line.strip().decode())
+                q.put((_loggername, line))
 
         def finish(self):
             self.request.close()
@@ -60,13 +75,14 @@ def syslog_server(host, port, poll_interval=0.5, loggername="slither.syslog", ce
 @click.option("--port", "-p", default=8014, type=int)
 @click.option("--poll-interval", "-i", default=0.5, type=float)
 @click.option("--loggername", "-n", default="{}.syslog".format(__name__))
+@click.option("--max-writers", "-w", default=5)
 @click.option("--cert", "-c", default=None, type=str)
 @click.option("--key", "-k", default=None, type=str)
 @click.option("--logging-config", "-l", default=None, type=str)
-def main(host, port, poll_interval, loggername, cert, key, logging_config):
+def main(host, port, poll_interval, loggername, max_writers, cert, key, logging_config):
     if logging_config:
         with open(logging_config, "r") as fin:
             logging.config.dictConfig(json.load(fin))
     else:
         logging.basicConfig(stream=sys.stdout, level=20)
-    syslog_server(host, port, poll_interval, loggername, cert, key)
+    syslog_server(host, port, poll_interval, loggername, max_writers, cert, key)
