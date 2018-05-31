a079430ccd94608694026c21f6299e1412919c73
ilovetux <me@ilovetux.com>
2018-05-31 11:23:54 -0400

    Add ability to specify stdin, also added --logging-config parameter

--- slither/slyce.py
+++ slither/slyce.py
@@ -10,21 +10,32 @@ logging.basicConfig(stream=sys.stdout, level=20, format="%(message)s")
 
 def handle_file(path, columns, patterns, content_type):
     log = logging.getLogger(__name__)
-    with path.open("r") as fin:
-        for line in fin:
+    if path is sys.stdin:
+        for line in path:
             if content_type == "xml":
                 line = etree.fromstring(line)
                 log.info(",".join(";".join(map(str, line.xpath(pattern))) for pattern in patterns))
             elif content_type == "text":
                 log.info(",".join(";".join(pattern.findall(line)) for pattern in patterns))
+    else:
+        with path.open("r") as fin:
+            for line in fin:
+                if content_type == "xml":
+                    line = etree.fromstring(line)
+                    log.info(",".join(";".join(map(str, line.xpath(pattern))) for pattern in patterns))
+                elif content_type == "text":
+                    log.info(",".join(";".join(pattern.findall(line)) for pattern in patterns))
 
 @click.command()
 @click.argument("src", type=click.Path())
 @click.option("--columns", "-c", multiple=True)
 @click.option("--xml", "content_type", flag_value="xml")
 @click.option("--text", "content_type", flag_value="text", default=True)
-def main(src, columns, content_type):
+@click.option("--logging-config", "-l", type=click.File(), default=None)
+def main(src, columns, content_type, logging_config):
     log = logging.getLogger(__name__)
+    if logging_config:
+        logging.dictConfig(json.load(logging_config))
     _columns, _patterns = [], []
     for column in columns:
         _column, _pattern = column.split(":", 1)
@@ -34,9 +45,12 @@ def main(src, columns, content_type):
         else:
             _patterns.append(_pattern)
     log.info(",".join(_columns))
-    for path in glob(src):
-        path = Path(path)
-        if path.is_file():
-            handle_file(path, _columns, _patterns, content_type)
-        elif path.is_dir():
-            handle_dir(path, columns, content_type)
+    if src == "-":
+        handle_file(sys.stdin, _columns, _patterns, content_type)
+    else:
+        for path in glob(src):
+            path = Path(path)
+            if path.is_file():
+                handle_file(path, _columns, _patterns, content_type)
+            elif path.is_dir():
+                handle_dir(path, columns, content_type)
